f8a1dd19c5a6a874aa76694056569d53c619e86a
ilkin3333 <ihumbetov09@gmail.com>
2024-04-30 11:01:58 +0400

    uzun listde skrol yaradildi

--- java.js
+++ java.js
@@ -1,22 +1,21 @@
 const inputBox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
-const terkib = inputBox.value
+// const terkib = inputBox.value
 
 function addTask() {
-    if (inputBox.value === '') {
+    if (inputBox.value.trim() === '') {
         alert('List boş qoyula bilməz!');
     }
-    else if(inputBox.value.length > 23) {
-        alert("List uzunluğu 23'dən kiçik olmalıdır!");
-    }
+
     else {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listcontainer.insertBefore(li, listcontainer.childNodes[0]);
-        
+
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
+        span.innerHTML = "\u00d7"; 
         li.appendChild(span);
+        span.style.backgroundColor = 'red'
     }
     inputBox.value = "";
     saveData();
